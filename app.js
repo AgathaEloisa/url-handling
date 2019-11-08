6d035fa8d2a7134004c44c409e0e222e352e9a9c
AgathaP <ebrumaktub@gmx.com>
2019-11-08 18:42:28 -0300

    agrego comentarios en app.js y cambio nombre a variables para que sea un poco más comprensible

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-
+//cambio la url por una frase
 const shortCutUrl = () => {
 let word = 'holi';
 // let pathname = window.location.href;
-let abc = encodeURI(word);
-return abc;
+let result = encodeURI(word);
+return result;
 }
 
+//Creo HTML
 document.getElementById('root').innerHTML = 
 `
 <button id="change-url">click me</button>
 <h1 id="url-text">${window.location.pathname}<h1>
 `
 
+//Hago que el evento click cambie el pathname por una frase
 document.getElementById('change-url').addEventListener('click', () => {
     document.getElementById('url-text').innerHTML = `${shortCutUrl()}`;
 })
